7951fb08fe3deb336537f9771e9e5a8d3510afe7
Bivekich <biveki-dev@yandex.ru>
2024-08-23 12:04:32 +0300

    vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sanity from '@sanity/astro';
-import netlify from '@astrojs/netlify';
+import vercel from "@astrojs/vercel/serverless";
 const sanityConfig = {
   projectId: 'lgd6f46p',
   dataset: 'production',
-  useCdn: true,
+  useCdn: true
 };
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sanity(sanityConfig)],
-  output: 'server',
-  adapter: netlify(),
+  output: "server",
+  adapter: vercel()
 });
